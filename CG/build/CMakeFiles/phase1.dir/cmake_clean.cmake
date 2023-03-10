2eecf67164c3cd4c7b01bf50ab0f936e38f45115
Gonçalo Freitas <73437644+gugafreitas@users.noreply.github.com>
2023-03-10 01:26:52 +0000

    Add files via upload

---
+++ CG/build/CMakeFiles/phase1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phase1.dir/engine.cpp.o"
+  "CMakeFiles/phase1.dir/engine.cpp.o.d"
+  "CMakeFiles/phase1.dir/tinyxml2.cpp.o"
+  "CMakeFiles/phase1.dir/tinyxml2.cpp.o.d"
+  "phase1"
+  "phase1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phase1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
